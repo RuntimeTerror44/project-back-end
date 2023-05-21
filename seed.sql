655cf2b4e3c325dac9ebe2271ebacaec2a7710bc
mohannaddQA <qa.mohannad@gmail.com>
2023-05-21 18:29:29 +0000

    adding comment table

--- seed.sql
+++ seed.sql
@@ -16,7 +16,7 @@ VALUES
 INSERT INTO jobs (userId, job_field, job_title, job_post_content, job_details)
 VALUES
     (1,'Information Technology', 'Software Engineer', 'Looking for a skilled software engineer', 'Responsibilities include developing software applications.'),
-    (1,'Information Technology', 'Software Engineer', 'Looking for a skilled software engineer', 'Responsibilities include developing software applications.')
+    (1,'Information Technology', 'Software Engineer', 'Looking for a skilled software engineer', 'Responsibilities include developing software applications.');
  
 -- POSTS table seed data
 INSERT INTO posts (user_id, paragraph_content, photo_content, post_date) VALUES
@@ -24,3 +24,9 @@ INSERT INTO posts (user_id, paragraph_content, photo_content, post_date) VALUES
     (1, 'I had a great day at the beach!', 'photo2.jpg', '2023-05-21'),
     (2, 'Excited for my upcoming trip!', 'photo3.jpg', '2023-05-22'),
     (3, 'Enjoying the beautiful sunset.', 'photo4.jpg', '2023-05-23');
+
+
+-- comments table seed 
+INSERT INTO comments (content, comment_date, user_id, post_id) VALUES
+('this is comment 1','2023-05-20',1,1 ),
+('this is comment 2','2023-05-21',2,2);
