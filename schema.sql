3ad5d8a517e445f5399f99b03fbba2c22c544dc6
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-05-22 19:22:32 +0000

    create-profile-getdata-from-auth0

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 -- USERS table
-DROP TABLE IF EXISTS usersinfo;
+DROP TABLE IF EXISTS usersinfo CASCADE;
 CREATE TABLE IF NOT EXISTS usersinfo (
     ID SERIAL PRIMARY KEY,
     username VARCHAR(50) ,
     password VARCHAR(50),
     firstName VARCHAR(50),
     lastName VARCHAR(50),
-    email VARCHAR(100) ,
+    email  VARCHAR (100) UNIQUE,
     dateofbirth DATE,
     country VARCHAR(50),
     city VARCHAR(50),
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS usersinfo (
 );
 
 -- PORTFOLIO table
-DROP TABLE IF EXISTS portfolio;
+DROP TABLE IF EXISTS portfolio ;
 CREATE TABLE IF NOT EXISTS portfolio (
   ID SERIAL PRIMARY KEY,
   userId INT,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS portfolio (
   languages TEXT
 );
 
-DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS jobs ;
 CREATE TABLE IF NOT EXISTS jobs(
 ID SERIAL PRIMARY KEY,
 userId INTEGER REFERENCES usersinfo(ID) ON DELETE CASCADE,
@@ -46,14 +46,14 @@ job_post_content VARCHAR(255),
 job_details VARCHAR(255)
 );
 -- POSTS table
-DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS posts ;
 CREATE TABLE IF NOT EXISTS posts (
   post_id SERIAL PRIMARY KEY,
   user_id INT,
   paragraph_content TEXT,
   photo_content TEXT,
   post_date DATE,
-  FOREIGN KEY (user_id) REFERENCES usersinfo (ID)
+  FOREIGN KEY (user_id) REFERENCES usersinfo (ID) ON DELETE CASCADE
 );
 
 
--- seed.sql
+++ seed.sql
@@ -9,18 +9,18 @@ VALUES
 -- Insert data into PORTFOLIO table
 INSERT INTO portfolio (userId, fullname, email, phonenum, address, country, profilepic, education, certifications, workexperience, skills, projects, languages)
 VALUES
-  (1, 'John Doe', 'johndoe@example.com', '1234567890', '123 Main St', 'USA', 'profile_pic_1.jpg', 'Bachelor of Science in Computer Science', 'Oracle Certified Java Programmer', 'Software Engineer at XYZ Company', 'Java, C++, Python', 'Project 1, Project 2', 'English, Spanish'),
-  (2, 'Jane Smith', 'janesmith@example.com', '9876543210', '456 Elm St', 'Canada', 'profile_pic_2.jpg', 'Master of Business Administration', 'Project Management Professional (PMP)', 'Project Manager at ABC Company', 'Project management, Leadership', 'Project 3, Project 4', 'English, French');
+  (1, 'John Doe', 'jonnnnhndoe@example.com', '1234567890', '123 Main St', 'USA', 'profile_pic_1.jpg', 'Bachelor of Science in Computer Science', 'Oracle Certified Java Programmer', 'Software Engineer at XYZ Company', 'Java, C++, Python', 'Project 1, Project 2', 'English, Spanish'),
+  (2, 'Jane Smith', 'janesmdfghjklith@example.com', '9876543210', '456 Elm St', 'Canada', 'profile_pic_2.jpg', 'Master of Business Administration', 'Project Management Professional (PMP)', 'Project Manager at ABC Company', 'Project management, Leadership', 'Project 3, Project 4', 'English, French');
 
 -- Insert data into JOBS table
 INSERT INTO jobs (userId, job_field, job_title, job_post_content, job_details)
 VALUES
     (1,'Information Technology', 'Software Engineer', 'Looking for a skilled software engineer', 'Responsibilities include developing software applications.'),
-    (1,'Information Technology', 'Software Engineer', 'Looking for a skilled software engineer', 'Responsibilities include developing software applications.')
+    (1,'Information Technology', 'Software Engineer', 'Looking for a skilled software engineer', 'Responsibilities include developing software applications.');
  
 -- POSTS table seed data
-INSERT INTO posts (user_id, paragraph_content, photo_content, post_date) VALUES
+INSERT INTO posts (user_id,paragraph_content, photo_content, post_date) VALUES
     (1, 'Hello, this is my first post!', 'photo1.jpg', '2023-05-20'),
     (1, 'I had a great day at the beach!', 'photo2.jpg', '2023-05-21'),
     (2, 'Excited for my upcoming trip!', 'photo3.jpg', '2023-05-22'),
-    (3, 'Enjoying the beautiful sunset.', 'photo4.jpg', '2023-05-23');
+    (2, 'Enjoying the beautiful sunset.', 'photo4.jpg', '2023-05-23');
